adfc47790a8d76f2cb91cd3e3c15c96eff78d0ed
sohanur1804 <sohanur1804@gmail.com>
2023-01-07 03:11:17 +0800

    part-6

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,8 +10,6 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     {{ __("You're logged in!") }}
-
-                    <livewire:counter />
                 </div>
             </div>
         </div>
