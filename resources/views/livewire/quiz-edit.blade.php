b6fa0ebed9768cd54773b49a7b880d77be351466
sohanur1804 <sohanur1804@gmail.com>
2023-01-21 02:42:05 +0800

    part-15

--- resources/views/livewire/quiz-edit.blade.php
+++ resources/views/livewire/quiz-edit.blade.php
@@ -1,12 +1,19 @@
 <div>
-    <h2>Questions</h2>
-    <h2>Add question</h2>
+
+    <h2 class="font-bold mb-2">Questions</h2>
+    <ul class="list-disc mb-2">
+        @foreach ($quiz->questions as $question)
+            <li>{{ $question->name }}</li>
+        @endforeach
+    </ul>
+    @if(count($questions) > 0)
+    <h2 class="font-bold mb-2">Add question</h2>
     <form wire:submit.prevent="addQuestion">
         <div class="mb-4">
             <label for="question_id" class="lms-label">Question</label>
             <select class="lms-input" wire:model.lazy="question_id" id="question_id">
                 <option value="">Select a question</option>
-                @foreach($questions as $question)
+                @foreach ($questions as $question)
                     <option value="{{ $question->id }}">{{ $question->name }}</option>
                 @endforeach
             </select>
@@ -14,4 +21,7 @@
 
         @include('components.wire-loading-btn')
     </form>
+    @else
+    no available questions
+    @endif
 </div>
